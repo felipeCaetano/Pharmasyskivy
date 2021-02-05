04acff32f247a27792aacc30ac1a34b9d7b3007a
felipe <felipecmelo@gmail.com>
2021-02-05 12:01:31 -0300

    update for MVC em medicamentos

---
+++ drivers/medicamentos_drive.py
@@ -0,0 +1,22 @@
+from kivymd.app import MDApp
+
+from controllers.medicamentos import MedicamentosController
+from models.medicamentos import Medicamento
+
+
+class MedicamentosApp(MDApp):
+
+    def __init__(self, **kwargs):
+        super(MedicamentosApp, self).__init__(**kwargs)
+        self.model = Medicamento()
+        self.controller = MedicamentosController(self.model)
+
+    def build(self):
+        return self.controller.get_screen()
+
+    def do_action(self, instance):
+        pass
+
+
+if __name__ == '__main__':
+    MedicamentosApp().run()
